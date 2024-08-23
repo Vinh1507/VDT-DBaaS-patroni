d71212f433515714fa08de14a71a31083b8be3ce
vinhbh <hoangvinh1577@gmail.com>
2024-08-23 13:41:59 +0700

    Update entrypoint for restapi/postgres connection

--- containerization/docker/entrypoint.sh
+++ containerization/docker/entrypoint.sh
@@ -15,50 +15,50 @@ readonly DOCKER_IP
 
 export DUMB_INIT_SETSID=0
 
-# case "$1" in
-#     haproxy)
-#         haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -D
-#         set -- confd "-prefix=$PATRONI_NAMESPACE/$PATRONI_SCOPE" -interval=10 -backend
-#         if [ -n "$PATRONI_ZOOKEEPER_HOSTS" ]; then
-#             while ! /usr/share/zookeeper/bin/zkCli.sh -server "$PATRONI_ZOOKEEPER_HOSTS" ls /; do
-#                 sleep 1
-#             done
-#             set -- "$@" zookeeper -node "$PATRONI_ZOOKEEPER_HOSTS"
-#         else
-#             while ! etcdctl member list 2> /dev/null; do
-#                 sleep 1
-#             done
-#             set -- "$@" etcdv3
-#             while IFS='' read -r line; do
-#                 set -- "$@" -node "$line"
-#             done <<-EOT
-# $(echo "$ETCDCTL_ENDPOINTS" | sed 's/,/\n/g')
-# EOT
-#         fi
-#         exec dumb-init "$@"
-#         ;;
-#     etcd)
-#         exec "$@" --auto-compaction-retention=1 -advertise-client-urls "http://$DOCKER_IP:2379"
-#         ;;
-#     zookeeper)
-#         exec /usr/share/zookeeper/bin/zkServer.sh start-foreground
-#         ;;
-# esac
+case "$1" in
+    haproxy)
+        haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -D
+        set -- confd "-prefix=$PATRONI_NAMESPACE/$PATRONI_SCOPE" -interval=10 -backend
+        if [ -n "$PATRONI_ZOOKEEPER_HOSTS" ]; then
+            while ! /usr/share/zookeeper/bin/zkCli.sh -server "$PATRONI_ZOOKEEPER_HOSTS" ls /; do
+                sleep 1
+            done
+            set -- "$@" zookeeper -node "$PATRONI_ZOOKEEPER_HOSTS"
+        else
+            while ! etcdctl member list 2> /dev/null; do
+                sleep 1
+            done
+            set -- "$@" etcdv3
+            while IFS='' read -r line; do
+                set -- "$@" -node "$line"
+            done <<-EOT
+$(echo "$ETCDCTL_ENDPOINTS" | sed 's/,/\n/g')
+EOT
+        fi
+        exec dumb-init "$@"
+        ;;
+    etcd)
+        exec "$@" --auto-compaction-retention=1 -advertise-client-urls "http://$DOCKER_IP:2379"
+        ;;
+    zookeeper)
+        exec /usr/share/zookeeper/bin/zkServer.sh start-foreground
+        ;;
+esac
 
-# ## We start an etcd
-# if [ -z "$PATRONI_ETCD3_HOSTS" ] && [ -z "$PATRONI_ZOOKEEPER_HOSTS" ]; then
-#     export PATRONI_ETCD_URL="http://127.0.0.1:2379"
-#     etcd --data-dir /tmp/etcd.data -advertise-client-urls=$PATRONI_ETCD_URL -listen-client-urls=http://0.0.0.0:2379 > /var/log/etcd.log 2> /var/log/etcd.err &
-# fi
+## We start an etcd
+if [ -z "$PATRONI_ETCD3_HOSTS" ] && [ -z "$PATRONI_ZOOKEEPER_HOSTS" ]; then
+    export PATRONI_ETCD_URL="http://127.0.0.1:2379"
+    etcd --data-dir /tmp/etcd.data -advertise-client-urls=$PATRONI_ETCD_URL -listen-client-urls=http://0.0.0.0:2379 > /var/log/etcd.log 2> /var/log/etcd.err &
+fi
 
 export PATRONI_SCOPE
 export PATRONI_NAMESPACE
 export PATRONI_NAME="${PATRONI_NAME:-$(hostname)}"
-export PATRONI_RESTAPI_CONNECT_ADDRESS="$DOCKER_IP:8008"
+export PATRONI_RESTAPI_CONNECT_ADDRESS="${PATRONI_RESTAPI_CONNECT_ADDRESS:-"$DOCKER_IP:8008"}"
 export PATRONI_RESTAPI_LISTEN="0.0.0.0:8008"
 export PATRONI_admin_PASSWORD="${PATRONI_admin_PASSWORD:-admin}"
 export PATRONI_admin_OPTIONS="${PATRONI_admin_OPTIONS:-createdb, createrole}"
-export PATRONI_POSTGRESQL_CONNECT_ADDRESS="$DOCKER_IP:5432"
+export PATRONI_POSTGRESQL_CONNECT_ADDRESS="${PATRONI_POSTGRESQL_CONNECT_ADDRESS:-"$DOCKER_IP:5432"}"
 export PATRONI_POSTGRESQL_LISTEN="0.0.0.0:5432"
 export PATRONI_POSTGRESQL_DATA_DIR="${PATRONI_POSTGRESQL_DATA_DIR:-$PGDATA}"
 export PATRONI_REPLICATION_USERNAME="${PATRONI_REPLICATION_USERNAME:-replicator}"
